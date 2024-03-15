92c0c9d0fcc7ee9a1a11683548225a822608313f
Ashik Lenin <ashikleninn@gamil.com>
2024-03-16 03:34:44 +0530

    changed done in home page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import Button from "../components/Button";
 import { arrowRight } from '../assets/icons';
-
+import { statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
 
 const Hero = () => {
   return (
@@ -9,20 +10,37 @@ const Hero = () => {
     className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  p-2'>
 
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-<p>Our Summer Collection</p>
-<h1>
-  <span>The New Arrival</span>
+<p className='text-2xl font-montserrat text-coral-red' >Our Summer Collection</p>
+<h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold'>
+  <span  className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>The New Arrival</span>
   <br />
-<span>Nike </span>
-Shoes
+<span className='text-coral-red inline-block mt-3'>Nike </span> Shoes
+
 </h1>
-<p>
+<p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, sed fugit, iusto saepe vel in est qui rerum sit 
 </p>
 <Button label= 'Shop now' 
 iconURL={ arrowRight } />
 
+<div className='flex justify-center items-center flex-wrap w-full mt-20 gap-16'>
+{ statistics.map((stat) =>(
+<div key={stat.label} >
+  <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+  <p className='leading-7 font-montserrat text-slate-gray'>{stat.label}</p>
+</div>
+))}
+</div>
       </div>
+<div className="">
+  <img 
+  src={bigShoe1} 
+  alt="shoe collection"
+  width={610}
+  height={500}
+  className="object-contain relative z-10" />
+</div>
+
 
     </section>
   )
