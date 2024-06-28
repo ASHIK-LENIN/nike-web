e58bf4639b0e7b3fe7edcfdc8680fa257d5a1eb7
Ashik Lenin <ashikleninn@gamil.com>
2024-06-29 00:01:26 +0530

    popular products & super quality section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,8 +4,12 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className='flex justify-center items-center gap-2  px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red  text-white rounded-full  border-coral-red'>
 {label}
-<img src={iconURL} alt="arrow right" 
+
+{iconURL && (
+  <img src={iconURL} alt="arrow right" 
 className='ml-2 rounded-full w-5 h-5'/>
+)}
+
     </button>
   )
 }
