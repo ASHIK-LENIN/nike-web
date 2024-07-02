12c3d36135a30405b03bf52860aa33079f2ae921
Ashik Lenin <ashikleninn@gamil.com>
2024-07-02 22:23:58 +0530

    Footer and rest completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import Hero from './sections/Hero'
-import PopularProducts from './sections/PopularProducts'
-import SuperQuality from './sections/SuperQuality'
-import Services from './sections/Services'
-import SpecialOffer from './sections/SpecialOffer'
-import Subscribe from './sections/Subscribe'
-import Footer from './sections/Footer'
-import CustomerReviews from './sections/CustomerReviews'
-import Nav from './components/Nav'
 
-import { Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav';
+import Hero from './sections/Hero';
+import PopularProducts from './sections/PopularProducts';
+import SuperQuality from './sections/SuperQuality';
+import Services from './sections/Services';
+import SpecialOffer from './sections/SpecialOffer';
+import CustomerReviews from './sections/CustomerReviews';
+import Subscribe from './sections/Subscribe';
+import Footer from './sections/Footer';
+
+
 
 const App = () => {
-
   return (
+    
     <main className='relative'>
-     <Nav />
-      <section className='xl:padding-l wide:padding-r padding-b'>
+      <Nav />
+      <section  className='xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
-
-      <section className='padding'>
+      <section  className='padding'>
         <PopularProducts />
       </section>
       <section className='padding'>
@@ -32,17 +31,17 @@ const App = () => {
       <section className='padding'>
         <SpecialOffer />
       </section>
-      <section className='padding bg-pale-blue'>
+      <section className='bg-pale-blue padding'>
         <CustomerReviews />
       </section>
       <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className='padding bg-black padding-x padding-t p'>
+      <section className=' bg-black padding-x padding-t pb-8'>
         <Footer />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
